df6b73f7a773a77fbf686e9a05c161023d201ba5
ckmine11 <167895891+ckmine11@users.noreply.github.com>
2024-04-27 12:03:20 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
-cd /var/lib/jenkins/workspace/mine-project/
+cd /var/lib/jenkins/workspace/devsecops-pro/
 sudo su - jenkins -s/bin/bash
-#docker logout   masternode.mine.com
-  docker login -u admin --password Harbor12345 masternode.mine.com
+#docker logout   master.dns.com
+  docker login -u admin --password Harbor12345 master.dns.com
 docker image build -t  $JOB_NAME:v1.$BUILD_ID .
- docker image tag $JOB_NAME:v1.$BUILD_ID masternode.mine.com/holder/$JOB_NAME:v1.$BUILD_ID
- docker image tag $JOB_NAME:v1.$BUILD_ID masternode.mine.com/holder/$JOB_NAME:latest
- docker image push masternode.mine.com/holder/$JOB_NAME:v1.$BUILD_ID
- docker  push masternode.mine.com/holder/$JOB_NAME:latest
- #docker image rmi $JOB_NAME:v1.$BUILD_ID  masternode.mine.com/holder/$JOB_NAME:v1.$BUILD_ID   masternode.mine.com/holder/$JOB_NAME:latest 
+ docker image tag $JOB_NAME:v1.$BUILD_ID master.dns.com/devsecops/$JOB_NAME:v1.$BUILD_ID
+ docker image tag $JOB_NAME:v1.$BUILD_ID master.dns.com/devsecops/$JOB_NAME:latest
+ docker image push master.dns.com/devsecops/$JOB_NAME:v1.$BUILD_ID
+ docker  push master.dns.com/devsecops/$JOB_NAME:latest
+ docker image rmi $JOB_NAME:v1.$BUILD_ID  master.dns.com/devsecops/$JOB_NAME:v1.$BUILD_ID   master.dns.com/devsecops/$JOB_NAME:latest 
