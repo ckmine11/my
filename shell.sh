cd6eed29de4d884bbcb63329e2e87037696cb528
ckmine11 <167895891+ckmine11@users.noreply.github.com>
2024-04-27 12:03:40 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -3,7 +3,7 @@ cd /var/lib/jenkins/workspace/devsecops-pro/
 sudo su - jenkins -s/bin/bash
 #docker logout   master.dns.com
   docker login -u admin --password Harbor12345 master.dns.com
-docker image build -t  $JOB_NAME:v1.$BUILD_ID .
+  docker image build -t  $JOB_NAME:v1.$BUILD_ID .
  docker image tag $JOB_NAME:v1.$BUILD_ID master.dns.com/devsecops/$JOB_NAME:v1.$BUILD_ID
  docker image tag $JOB_NAME:v1.$BUILD_ID master.dns.com/devsecops/$JOB_NAME:latest
  docker image push master.dns.com/devsecops/$JOB_NAME:v1.$BUILD_ID
