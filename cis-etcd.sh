920d8982903d499d94b90fb4d6d6613855174511
ckmine11 <167895891+ckmine11@users.noreply.github.com>
2024-04-30 16:25:48 +0530

    Update cis-etcd.sh

--- cis-etcd.sh
+++ cis-etcd.sh
@@ -3,7 +3,7 @@
 
 total_fail=$(kube-bench run --targets etcd  --version 1.28 --check 2.2 --json | jq .Totals.total_fail)
 
-if [[ "$total_fail" -ne 0 ]];
+if [[ "$total_fail" -ne 1 ]];
         then
                 echo "CIS Benchmark Failed ETCD while testing for 2.2"
                 exit 1;
