29b3d288819f47b0cee6b33c8925451ea04316c3
ckmine11 <167895891+ckmine11@users.noreply.github.com>
2024-07-27 05:21:10 -0700

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
-cd /var/lib/jenkins/workspace/devsecops-pro/
+cd /var/lib/jenkins/workspace/sample-project/
 sudo su - jenkins -s/bin/bash
-#docker logout   master.dns.com
-  docker login -u admin --password Harbor12345 master.dns.com
+#docker logout   harbor.cluster.com/sample-project
+  docker login -u admin --password root harbor.cluster.com
   docker image build -t  $JOB_NAME:v1.$BUILD_ID .
- docker image tag $JOB_NAME:v1.$BUILD_ID master.dns.com/devsecops/$JOB_NAME:v1.$BUILD_ID
- docker image tag $JOB_NAME:v1.$BUILD_ID master.dns.com/devsecops/$JOB_NAME:latest
- docker image push master.dns.com/devsecops/$JOB_NAME:v1.$BUILD_ID
- docker  push master.dns.com/devsecops/$JOB_NAME:latest
- docker image rmi $JOB_NAME:v1.$BUILD_ID  master.dns.com/devsecops/$JOB_NAME:v1.$BUILD_ID   master.dns.com/devsecops/$JOB_NAME:latest 
+ docker image tag $JOB_NAME:v1.$BUILD_ID harbor.cluster.com/sample-project/$JOB_NAME:v1.$BUILD_ID
+ docker image tag $JOB_NAME:v1.$BUILD_ID harbor.cluster.com/sample-project/$JOB_NAME:latest
+ docker image push harbor.cluster.com/sample-project/$JOB_NAME:v1.$BUILD_ID
+ docker  push harbor.cluster.com/sample-project/$JOB_NAME:latest
+ docker image rmi $JOB_NAME:v1.$BUILD_ID  harbor.cluster.com/sample-project/$JOB_NAME:v1.$BUILD_ID   harbor.cluster.com/sample-project/$JOB_NAME:latest 
