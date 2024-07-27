49e878edd2448d8a5ee5e67fb401fc24716d1e27
ckmine11 <167895891+ckmine11@users.noreply.github.com>
2024-07-27 05:44:31 -0700

    Update trivy-k8s-scan.sh

--- trivy-k8s-scan.sh
+++ trivy-k8s-scan.sh
@@ -3,15 +3,15 @@
 
 echo $imageName #getting Image name from env variable
 
-docker run --rm   -v  /var/run/docker.sock:/var/run/docker.sock -v $WORKSPACE:/root/.cache/ aquasec/trivy -q image --reset --exit-code 0 --severity LOW,MEDIUM,HIGH --light $imageName
-docker run --rm  -v  /var/run/docker.sock:/var/run/docker.sock -v $WORKSPACE:/root/.cache/ aquasec/trivy -q image --reset --exit-code 1 --severity CRITICAL --light $imageName
+docker run --rm   -v  /var/run/docker.sock:/var/run/docker.sock -v $WORKSPACE:/root/.cache/ aquasec/trivy -q image  --exit-code 0 --severity LOW,MEDIUM,HIGH --light $imageName
+docker run --rm  -v  /var/run/docker.sock:/var/run/docker.sock -v $WORKSPACE:/root/.cache/ aquasec/trivy -q image  --exit-code 1 --severity CRITICAL --light $imageName
 
     # Trivy scan result processing
     exit_code=$?
     echo "Exit Code : $exit_code"
 
     # Check scan results
-    if [[ ${exit_code} == 3 ]]; then
+    if [[ ${exit_code} == 1 ]]; then
         echo "Image scanning failed. Vulnerabilities found"
         exit 1;
     else
